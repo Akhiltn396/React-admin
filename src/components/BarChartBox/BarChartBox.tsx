9f9eda8aaac5f4af5b3aed8dce056792332ce1f7
Akhil-396 <81629296+Akhil-396@users.noreply.github.com>
2023-09-03 09:34:56 +0530

    added barchart

--- src/components/BarChartBox/BarChartBox.tsx
+++ src/components/BarChartBox/BarChartBox.tsx
@@ -1,15 +1,5 @@
-import React from "react";
-import {
-  BarChart,
-  Bar,
-  Cell,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
+import "./barChartBox.scss";
 
 type Props = {
   title: string;
@@ -17,12 +7,13 @@ type Props = {
   dataKey: string;
   chartData: object[];
 };
+
 const BarChartBox = (props: Props) => {
   return (
     <div className="barChartBox">
       <h1>{props.title}</h1>
       <div className="chart">
-        <ResponsiveContainer width="99%" height="100%">
+        <ResponsiveContainer width="99%" height={150}>
           <BarChart data={props.chartData}>
             <Tooltip
               contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
